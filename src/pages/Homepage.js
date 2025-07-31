30a4d2ab6fa6118ba462db23e499340057c9f186
jungmin <imcha@lxpantospartner.com>
2025-07-31 15:33:41 +0900

    commit test

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const HomePage = () => {
     return (
         <div>
-            <h1>환영합니다</h1>
-            <Link to="/board">게시판으로 이동</Link>
+            <h1>환영합니다. 여기는 메인홈입니다.</h1>
+            <Link to="/board">--->게시판으로 이동</Link>
         </div>
     );
 };
